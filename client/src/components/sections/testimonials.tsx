317c1635161084e56ee9f01a84cf2279d0f146d6
vaden89 <isaacshosanya89@gmail.com>
2025-11-09 16:46:27 +0100

    feat: created pages for the products page, responsiveness on the testimonial section

--- client/src/components/sections/testimonials.tsx
+++ client/src/components/sections/testimonials.tsx
@@ -1,23 +1,49 @@
+"use client";
 import { ArrowLeft, ArrowRight, BadgeCheckIcon } from "lucide-react";
 import Image from "next/image";
+import { useRef } from "react";
 
 export const TestimonialSection = () => {
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
+  const scroll = (direction: "left" | "right") => {
+    if (scrollContainer.current) {
+      const container = scrollContainer.current;
+      const scrollAmount = container.clientWidth / 2;
+
+      container.scrollTo({
+        left: direction === "left" ? -scrollAmount : scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <section className="w-full flex flex-col p-5 gap-5">
       <div className="w-full flex justify-between items-end">
-        <span className="text-[32px] leading-[36px] font-bold">
-          OUR HAPPY <br /> CUSTOMERS
+        <span className="text-[32px] leading-9 font-bold">
+          OUR HAPPY <br className="sm:hidden" /> CUSTOMERS
         </span>
-        <div className="flex items-center gap-4">
-          <button aria-label="Previous Testimonial">
+        <div className="hidden sm:flex items-center gap-4">
+          <button
+            onClick={() => scroll("left")}
+            aria-label="Previous Testimonial"
+          >
             <ArrowLeft strokeWidth={3} />
           </button>
-          <button aria-label="Next Testimonial">
+          <button onClick={() => scroll("right")} aria-label="Next Testimonial">
             <ArrowRight strokeWidth={3} />
           </button>
         </div>
       </div>
-      <div>
+      <div
+        ref={scrollContainer}
+        className="w-full flex gap-4 overflow-x-auto hide-scroll"
+      >
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
         <TestimonialCard />
       </div>
     </section>
@@ -27,7 +53,7 @@ export const TestimonialSection = () => {
 const TestimonialCard = () => {
   const arr = Array.from({ length: 5 });
   return (
-    <div className="w-full min-h-[180px] flex flex-col gap-3 p-6 border rounded-xl">
+    <div className="w-full sm:w-1/4 min-h-[180px] shrink-0 flex flex-col gap-3 p-6 border rounded-xl">
       <div className="flex">
         {arr.map((item, index) => (
           <Image
